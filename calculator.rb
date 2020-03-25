c9d460089efd0fc78d467d2c868a1e3f9b6b955b
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-25 19:36:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,24 @@
 # Add your variables here
-def first_number = 0
+def first_number
+  puts 0
+end
 
-def second_number = 1
-  
-def sum = (first_number + second_number)
-  
-def difference = ()
+def second_number
+  puts 1
+end
+
+def sum 
+  puts (first_number + second_number)
+end 
+
+def difference
+  puts (first_number - second_number)
+end
+
+def product
+  puts (first_number*second_number)
+end
+
+def quotient
+  puts (first_number%second_number)
+end
