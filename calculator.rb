b6d994b7d762afb0cfcaeda801aad045719d0077
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-25 19:44:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,16 +2,6 @@
 first_number = 3
 second_number = 1
 sum = first_number + second_number
-
-
-def difference
-  puts (first_number - second_number)
-end
-
-def product
-  puts (first_number * second_number)
-end
-
-def quotient
-  puts (first_number % second_number)
-end
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number % second_number
